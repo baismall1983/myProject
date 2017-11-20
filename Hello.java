58948b7dabfc0a47ba54937a8419e5c22885cd1f
baichaoda <81010512@qq.com>
2017-11-20 16:16:19 +0800

    add hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @author baichaoda
+ * @ClassName: Hello
+ * @Description: test java
+ * @date 2017-11-20
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello java!");
+    }
+}
